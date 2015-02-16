acadadf7f4a4ad0f6da2b0912bd5592129941de2
AK <kozovski.a@gmail.com>
2015-02-16 15:22:04 -0500

    adding directory

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://my-ghost-blog.com',
+        url: 'https://floating-mountain-6379.herokuapp.com',
         mail: {},
         database: {
             client: 'postgres',
