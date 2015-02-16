b4636ab9c7cdd5a8c4dfb0e9de682a86fee0ed03
AK <kozovski.a@gmail.com>
2015-02-16 14:51:49 -0500

    postgres issue2

--- config.js
+++ config.js
@@ -14,11 +14,13 @@ config = {
         mail: {},
         database: {
             client: 'postgres',
-            host: process.env.POSTGRES_HOST,
-                       user: process.env.POSTGRES_USER,
-                       password: process.env.POSTGRES_PASSWORD,
-                       database: process.env.POSTGRES_DATABASE,
-                       port: '5432'
+            connection: {
+              host: process.env.POSTGRES_HOST,
+              user: process.env.POSTGRES_USER,
+              password: process.env.POSTGRES_PASSWORD,
+              database: process.env.POSTGRES_DATABASE,
+              port: '5432'
+
             },
             debug: false
         },
